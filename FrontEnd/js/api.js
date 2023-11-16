7b005c70dc291bcb67f23634c157ea8d50ee0162
LabatiJulien <julien.labati@hotmail.fr>
2023-11-16 15:26:24 +0100

    essais js/api

--- FrontEnd/js/api.js
+++ FrontEnd/js/api.js
@@ -1,25 +1,19 @@
-const apiUrl = 'http://localhost:5678/api/works';
+async function fetchData() {
+  const apiUrl = "http://localhost:5678/api/works";
+  try {
+    const response = await fetch(apiUrl);
 
-// Fonction pour récupérer les projets depuis le backend
-async function fetchProjects() {
-    try {
-      // Utilisation de fetch pour faire la requête à l'API
-      const response = await fetch(apiUrl);
-  
-      // Vérification si la requête a réussi (statut 200 OK)
-      if (!response.ok) {
-        throw new Error('Erreur réseau ou serveur');
-      }
-  
-      // Conversion de la réponse en JSON
+    if (response.ok) {
       const data = await response.json();
-  
-      // Utilisation des données récupérées (remplacez cela par ce que vous devez faire)
-      console.log('Projets récupérés :', data);
-    } catch (error) {
-      console.error('Erreur lors de la récupération des données :', error);
+      console.log("La requête vers l'API a réussi. Statut :", response.status);
+      console.log("Données récupérées de l'API :", data);
+    } else {
+      console.error("Erreur lors de la récupération des données de l'API. Statut :", response.status);
     }
+  } catch (error) {
+    console.error("Une erreur s'est produite lors de la requête :", error);
   }
-  
-  // Appel de la fonction pour récupérer les projets
-  fetchProjects();
+}
+
+// Appel de la fonction asynchrone
+fetchData();
