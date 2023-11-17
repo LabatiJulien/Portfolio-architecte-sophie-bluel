a0668ab7295843e6b1f7bb97cfd93f9303c54a06
LabatiJulien <julien.labati@hotmail.fr>
2023-11-17 10:52:09 +0100

    Récupération dyamique de l'api/allégement html

--- FrontEnd/js/api.js
+++ FrontEnd/js/api.js
@@ -7,6 +7,9 @@ async function fetchData() {
       const data = await response.json();
       console.log("La requête vers l'API a réussi. Statut :", response.status);
       console.log("Données récupérées de l'API :", data);
+      
+      displayGalleryItems(data);
+  
     } else {
       console.error("Erreur lors de la récupération des données de l'API. Statut :", response.status);
     }
@@ -15,5 +18,21 @@ async function fetchData() {
   }
 }
 
+function displayGalleryItems(data) {
+  const galleryDiv = document.getElementById("gallery");
+  galleryDiv.innerHTML = ""
+  data.forEach(item => {
+    const figureElement = document.createElement("figure");
+    const imgElement = document.createElement("img");
+    imgElement.src = item.imageUrl;
+    imgElement.alt = item.title;
+    const figcaptionElement = document.createElement("figcaption");
+    figcaptionElement.textContent = item.title;
+
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(figcaptionElement);
+    galleryDiv.appendChild(figureElement);
+  });
+}
 // Appel de la fonction asynchrone
 fetchData();
