5d3416b25100c19bef588c759a7597d947db1467
LabatiJulien <julien.labati@hotmail.fr>
2023-11-17 11:25:22 +0100

    Ajout des filtres Js

--- FrontEnd/js/api.js
+++ FrontEnd/js/api.js
@@ -1,3 +1,39 @@
+// Déclaration de la fonction pour afficher dynamiquement les travaux
+function displayGalleryItems(data) {
+  const galleryDiv = document.getElementById("gallery");
+  galleryDiv.innerHTML = "";
+
+  data.forEach(item => {
+    const figureElement = document.createElement("figure");
+    const imgElement = document.createElement("img");
+    imgElement.src = item.imageUrl;
+    imgElement.alt = item.title;
+
+    const figcaptionElement = document.createElement("figcaption");
+    figcaptionElement.textContent = item.title;
+
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(figcaptionElement);
+
+    galleryDiv.appendChild(figureElement);
+  });
+}
+
+// Déclaration de la fonction pour filtrer les travaux par catégorie
+function filterByCategory(categoryId) {
+  // Récupère tous les travaux depuis la variable globale
+  const allWorks = window.allWorks || [];
+
+  // Filtrer les travaux en fonction de la catégorie
+  const filteredWorks = (categoryId === 'all') ?
+    allWorks :
+    allWorks.filter(work => work.categoryId === categoryId);
+
+  // Appel de la fonction pour afficher dynamiquement les travaux filtrés dans la galerie
+  displayGalleryItems(filteredWorks);
+}
+
+// Déclaration de la fonction asynchrone pour récupérer les données de l'API
 async function fetchData() {
   const apiUrl = "http://localhost:5678/api/works";
   try {
@@ -7,9 +43,12 @@ async function fetchData() {
       const data = await response.json();
       console.log("La requête vers l'API a réussi. Statut :", response.status);
       console.log("Données récupérées de l'API :", data);
-      
+
+      // Appel de la fonction pour afficher dynamiquement les travaux
       displayGalleryItems(data);
-  
+
+      // Stocke les données pour les utiliser lors du filtrage
+      window.allWorks = data;
     } else {
       console.error("Erreur lors de la récupération des données de l'API. Statut :", response.status);
     }
@@ -18,21 +57,24 @@ async function fetchData() {
   }
 }
 
-function displayGalleryItems(data) {
-  const galleryDiv = document.getElementById("gallery");
-  galleryDiv.innerHTML = ""
-  data.forEach(item => {
-    const figureElement = document.createElement("figure");
-    const imgElement = document.createElement("img");
-    imgElement.src = item.imageUrl;
-    imgElement.alt = item.title;
-    const figcaptionElement = document.createElement("figcaption");
-    figcaptionElement.textContent = item.title;
-
-    figureElement.appendChild(imgElement);
-    figureElement.appendChild(figcaptionElement);
-    galleryDiv.appendChild(figureElement);
+document.addEventListener("DOMContentLoaded", function() {
+  // Ajout des gestionnaires d'événements pour le filtrage par catégorie
+  document.getElementById("allButton").addEventListener("click", function() {
+    filterByCategory('all');
   });
-}
-// Appel de la fonction asynchrone
-fetchData();
+
+  document.getElementById("objectsButton").addEventListener("click", function() {
+    filterByCategory(1);
+  });
+
+  document.getElementById("apartmentsButton").addEventListener("click", function() {
+    filterByCategory(2);
+  });
+
+  document.getElementById("hotelsButton").addEventListener("click", function() {
+    filterByCategory(3);
+  });
+
+  // Appel de la fonction asynchrone pour récupérer les données de l'API
+  fetchData();
+});
