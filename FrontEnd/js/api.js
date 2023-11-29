bac05f890e2f89cbe9785ad1eac91a340bb21a39
LabatiJulien <julien.labati@hotmail.fr>
2023-11-29 14:20:39 +0100

    récupération dynamique dans la modale

--- FrontEnd/js/api.js
+++ FrontEnd/js/api.js
@@ -64,6 +64,48 @@ async function fetchData() {
   }
 }
 
+function toggleEditModeBanner(isEditMode) {
+  const header = document.querySelector("header");
+
+  // Vérifie si l'élément du header existe
+  if (header) {
+    // Vérifie si le mode édition est activé
+    if (isEditMode) {
+      // Crée la bande noire avec le texte "Mode édition"
+      const editModeBanner = document.createElement("div");
+      editModeBanner.className = "edit-mode-banner";
+
+      // Crée l'icône Font Awesome
+      const iconElement = document.createElement("i");
+      iconElement.classList.add("fa-regular", "fa-pen-to-square", "fa-xs");
+
+      // Ajoute l'icône à la bannière
+      editModeBanner.appendChild(iconElement);
+
+      // Crée l'élément pour le texte "Mode édition"
+      const textElement = document.createElement("span");
+      textElement.textContent = " Mode édition";
+
+      // Ajoute le texte à la bannière
+      editModeBanner.appendChild(textElement);
+
+      // Ajoute la bande noire au-dessus du header
+      header.parentNode.insertBefore(editModeBanner, header);
+
+      // Ajoute une classe au header pour ajuster la position
+      header.classList.add("header-with-banner");
+    } else {
+      // Supprime la bande noire et la classe du header
+      const editModeBanner = document.querySelector(".edit-mode-banner");
+      if (editModeBanner) {
+        editModeBanner.remove();
+      }
+
+      header.classList.remove("header-with-banner");
+    }
+  }
+}
+
 // Déclaration de la fonction pour vérifier si l'utilisateur est connecté
 function isLoggedIn() {
   const token = localStorage.getItem('token');
@@ -116,6 +158,144 @@ document.addEventListener("DOMContentLoaded", async function () {
       // Vérifie si un token est présent dans le stockage local
       const token = localStorage.getItem('token');
 
+      // Ajout des gestionnaires d'événements pour le filtrage par catégorie
+      const allButton = document.getElementById("allButton");
+      const objectsButton = document.getElementById("objectsButton");
+      const apartmentsButton = document.getElementById("apartmentsButton");
+      const hotelsButton = document.getElementById("hotelsButton");
+
+      if (allButton) {
+        allButton.addEventListener("click", function () {
+          filterByCategory('all');
+        });
+      }
+
+      if (objectsButton) {
+        objectsButton.addEventListener("click", function () {
+          filterByCategory(1);
+        });
+      }
+
+      if (apartmentsButton) {
+        apartmentsButton.addEventListener("click", function () {
+          filterByCategory(2);
+        });
+      }
+
+      if (hotelsButton) {
+        hotelsButton.addEventListener("click", function () {
+          filterByCategory(3);
+        });
+      }
+
+      // Ajout de la modale
+      const modalTriggers = document.querySelectorAll(".modal-btn.modal-trigger");
+      const overlay = document.querySelector(".overlay");
+      const galleryContent = document.getElementById("gallery");
+
+      // Ajoutez un gestionnaire d'événements persistant pour le clic sur l'overlay
+overlay.addEventListener("click", overlayClickHandler);
+
+      // Vérifie si l'utilisateur est connecté
+      const isUserLoggedIn = isLoggedIn();
+
+      // Affiche ou masque le bouton "modifier" en fonction de la connexion
+      modalTriggers.forEach(trigger => {
+        trigger.style.display = isUserLoggedIn ? "block" : "none";
+      });
+
+      // Ajout des gestionnaires d'événements de la modal
+      modalTriggers.forEach(trigger => {
+        trigger.addEventListener("click", function () {
+          console.log("Modal Trigger Clicked");
+          toggleModal();
+          displayGalleryContent();
+        });
+      });
+
+      const closeModalButton = document.querySelector(".close-modal");
+      if (closeModalButton) {
+        closeModalButton.addEventListener("click", function () {
+          console.log("Close Modal Clicked");
+          toggleModal();
+        });
+      }
+
+      function toggleModal() {
+        console.log("Toggle Modal");
+        const modalContainer = document.querySelector(".modal-container");
+        const overlay = document.querySelector(".overlay");
+    
+        // Inverse la classe pour afficher ou masquer la modale
+        modalContainer.classList.toggle("active");
+    
+        if (modalContainer.classList.contains("active")) {
+            // Ajoute un gestionnaire d'événements pour fermer la modale en cliquant sur l'overlay
+            overlay.addEventListener("click", overlayClickHandler);
+        } else {
+            // Supprime le gestionnaire d'événements lorsqu'on ferme la modale
+            overlay.removeEventListener("click", overlayClickHandler);
+        }
+    }
+    
+    function overlayClickHandler() {
+        console.log("Overlay Clicked");
+        console.log("Toggle Modal from overlayClickHandler");
+        toggleModal();
+    }
+    
+    function overlayClickHandler(event) {
+      console.log("Overlay Clicked");
+      console.log("Event target:", event.target);
+      console.log("Overlay:", overlay);
+      
+      if (event.target.classList.contains('overlay')) {
+          console.log("Toggle Modal from overlayClickHandler");
+          toggleModal();
+      }
+  }
+  
+  function displayGalleryContent() {
+    // Efface le contenu existant de la modale
+    const modal = document.querySelector(".modal");
+    modal.innerHTML = "";
+  
+    // Crée un conteneur pour le contenu de la modale
+    const modalContent = document.createElement("div");
+  
+    // Ajoute le titre h1 à la modale
+    const title = document.createElement("h1");
+    title.textContent = "Galerie photo";
+    modalContent.appendChild(title);
+  
+    // Clone le contenu de la galerie et l'ajoute au conteneur de la modale
+    const galleryClone = galleryContent.cloneNode(true);
+    galleryClone.classList.add("modal-gallery");
+    modalContent.appendChild(galleryClone);
+  
+    // Ajoute le bouton "Ajouter une photo" à la modale
+    const addButton = document.createElement("button");
+    addButton.id = "boutonAjoutdePhoto";
+    addButton.className = "modal-button";
+    addButton.textContent = "Ajouter une photo";
+    modalContent.appendChild(addButton);
+  
+    // Ajoute le conteneur de la modale à la modale
+    modal.appendChild(modalContent);
+  
+    // Ajoute un gestionnaire d'événements pour fermer la modale
+    const closeModalButton = document.createElement("button");
+    closeModalButton.className = "close-modal modal-trigger";
+    closeModalButton.textContent = "X";
+    closeModalButton.addEventListener("click", function () {
+      console.log("Close Modal Clicked");
+      toggleModal();
+    });
+  
+    modal.appendChild(closeModalButton);
+  }
+  
+   
       if (token) {
         // Utilisateur connecté
         fetchData();
@@ -146,109 +326,12 @@ document.addEventListener("DOMContentLoaded", async function () {
       }
     }
   }
-  
-  // Ajout des gestionnaires d'événements pour le filtrage par catégorie
-  const allButton = document.getElementById("allButton");
-  const objectsButton = document.getElementById("objectsButton");
-  const apartmentsButton = document.getElementById("apartmentsButton");
-  const hotelsButton = document.getElementById("hotelsButton");
-
-  if (allButton) {
-    allButton.addEventListener("click", function () {
-      filterByCategory('all');
-    });
+  function overlayClickHandler() {
+    console.log("Overlay Clicked");
+    console.log("Toggle Modal from overlayClickHandler");
+    toggleModal();
   }
-
-  if (objectsButton) {
-    objectsButton.addEventListener("click", function () {
-      filterByCategory(1);
-    });
-  }
-
-  if (apartmentsButton) {
-    apartmentsButton.addEventListener("click", function () {
-      filterByCategory(2);
-    });
-  }
-
-  if (hotelsButton) {
-    hotelsButton.addEventListener("click", function () {
-      filterByCategory(3);
-    });
-  }
-});
-
-// Ajout de la modale
-window.onload = function () {
-  const modalContainer = document.querySelector(".modal-container");
-  const modalTriggers = document.querySelectorAll(".modal-btn.modal-trigger");
-  const closeModalButtons = document.querySelectorAll(".close-modal.modal-trigger");
+  // Ajoutez un gestionnaire d'événements persistant pour le clic sur l'overlay
   const overlay = document.querySelector(".overlay");
-
-  // Vérifie si l'utilisateur est connecté
-  const isUserLoggedIn = isLoggedIn();
-
-  // Affiche ou masque le bouton "modifier" en fonction de la connexion
-  modalTriggers.forEach(trigger => {
-    trigger.style.display = isUserLoggedIn ? "block" : "none";
-    trigger.removeEventListener("click", toggleModal);
-    trigger.addEventListener("click", toggleModal);
-  });
-
-  closeModalButtons.forEach(button => button.addEventListener("click", toggleModal));
-
-  // Ajout de la vérification pour éviter l'erreur si overlay est null
-  if (overlay) {
-    overlay.addEventListener("click", function (event) {
-      if (event.target === overlay) {
-        toggleModal();
-      }
-    });
-  }
-
-  function toggleModal() {
-    modalContainer.classList.toggle("active");
-  }
-};
-function toggleEditModeBanner(isEditMode) {
-  const header = document.querySelector("header");
-
-  // Vérifie si l'élément du header existe
-  if (header) {
-    // Vérifie si le mode édition est activé
-    if (isEditMode) {
-      // Crée la bande noire avec le texte "Mode édition"
-      const editModeBanner = document.createElement("div");
-      editModeBanner.className = "edit-mode-banner";
-
-      // Crée l'icône Font Awesome
-      const iconElement = document.createElement("i");
-      iconElement.classList.add("fa-regular", "fa-pen-to-square", "fa-xs");
-
-      // Ajoute l'icône à la bannière
-      editModeBanner.appendChild(iconElement);
-
-      // Crée l'élément pour le texte "Mode édition"
-      const textElement = document.createElement("span");
-      textElement.textContent = " Mode édition";
-
-      // Ajoute le texte à la bannière
-      editModeBanner.appendChild(textElement);
-
-      // Ajoute la bande noire au-dessus du header
-      header.parentNode.insertBefore(editModeBanner, header);
-
-      // Ajoute une classe au header pour ajuster la position
-      header.classList.add("header-with-banner");
-    } else {
-      // Supprime la bande noire et la classe du header
-      const editModeBanner = document.querySelector(".edit-mode-banner");
-      if (editModeBanner) {
-        editModeBanner.remove();
-      }
-
-      header.classList.remove("header-with-banner");
-    }
-  }
-}
-
+  overlay.addEventListener("click", overlayClickHandler);
+});
